5d857f3da27d8512d0ab6a4cbbb8d733b4e58c6c
bogdan_primakovski <primakovski1995@gmail.com>
2016-06-10 18:05:46 +0300

    added /timetables for test_univer_bot

--- sh/core.sh
+++ sh/core.sh
@@ -6,4 +6,4 @@ apt-get install -y git build-essential
 # Install Pillow external libraries
 apt-get install -y libtiff4-dev libjpeg8-dev zlib1g-dev \
         libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev \
-        tk8.5-dev python-tk libffi-dev libxml2-dev libtool
+        tk8.5-dev python-tk libffi-dev libxml2-dev libtool httpie
